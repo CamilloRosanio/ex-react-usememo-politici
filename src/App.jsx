540eccdeb6f0266cfa716e06cea5ca5305a588e3
MyUsername <rosanio.c@gmail.com>
2025-03-19 18:29:12 +0100

    Milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import { useState, useEffect } from "react";
+
 function App() {
+
+  const [politicians, setPoliticians] = useState([]);
+
+  useEffect(() => {
+    fetch('https://boolean-spec-frontend.vercel.app/freetestapi/politicians')
+      .then(res => res.json())
+      .then(data => setPoliticians(data))
+      .catch(err => console.error(err));
+  }, [])
+
+  console.log(politicians);
 
   return (
     <>
-      App content here
+      <h1 className="debug">Lista politici</h1>
+      <div>
+        {politicians.map(p =>
+          <div key={p.id} className="debug">
+            <img src={p.image} alt={p.name} />
+            <h3>{p.name}</h3>
+            <h4>{p.position}</h4>
+            <p>{p.biography}</p>
+          </div>
+        )}
+      </div>
     </>
   )
 }
